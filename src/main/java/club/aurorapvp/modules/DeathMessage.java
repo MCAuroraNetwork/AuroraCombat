0fcdeabbbc6fa18509d7631ab88998a656dd58c7
Villagers654 <thbvill@gmail.com>
2023-04-17 20:44:15 +0900

    Make melee weapon hoverable

--- src/main/java/club/aurorapvp/modules/DeathMessage.java
+++ src/main/java/club/aurorapvp/modules/DeathMessage.java
@@ -2,10 +2,10 @@ package club.aurorapvp.modules;
 
 import static club.aurorapvp.AuroraCombat.COMPONENT_SERIALIZER;
 
-import club.aurorapvp.AuroraCombat;
 import club.aurorapvp.configs.Lang;
 import club.aurorapvp.events.custom.PlayerKilledByPlayerEvent;
 import club.aurorapvp.util.MaterialUtil;
+import net.kyori.adventure.text.event.HoverEvent;
 import org.bukkit.block.BlockState;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Projectile;
@@ -17,12 +17,17 @@ public class DeathMessage {
     switch (event.getDamageType()) {
       case MELEE -> {
         ItemStack weapon = (ItemStack) event.getWeapon();
+        HoverEvent<HoverEvent.ShowItem> hover = weapon.asHoverEvent();
 
         event.deathMessage(
             Lang.formatComponent("death-message.killed-by-player-slain",
-                event.getDamaged().getName(), event.getDamager().getName(),
-                COMPONENT_SERIALIZER.serialize(weapon.displayName())));
+                    event.getDamaged().getName(),
+                    event.getDamager().getName(),
+                    COMPONENT_SERIALIZER.serialize(weapon.displayName()))
+                .hoverEvent(hover)
+        );
       }
+
       case EXPLOSION_ENTITY -> {
         Entity weapon = (Entity) event.getWeapon();
 
