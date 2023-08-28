bb0a3c918b061620e287b8f0737c3734eb2ea365
Villagers654 <thbvill@gmail.com>
2023-08-28 19:06:20 +0900

    Pretty colors

--- src/main/java/club/aurorapvp/auroracombat/util/ItemStackUtil.java
+++ src/main/java/club/aurorapvp/auroracombat/util/ItemStackUtil.java
@@ -1,6 +1,8 @@
 package club.aurorapvp.auroracombat.util;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.EnderCrystal;
@@ -9,8 +11,6 @@ import org.bukkit.entity.SpectralArrow;
 import org.bukkit.entity.ThrownPotion;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.inventory.meta.PotionMeta;
-import org.bukkit.potion.PotionEffect;
 
 public class ItemStackUtil {
 
@@ -30,22 +30,12 @@ public class ItemStackUtil {
   public static ItemStack toItemStack(Projectile projectile) {
     ItemStack item;
     Component displayName = projectile.name();
+    displayName = displayName.colorIfAbsent(NamedTextColor.AQUA);
 
-    if (projectile instanceof Arrow arrow) {
-      if (!arrow.getCustomEffects().isEmpty()) {
-        item = new ItemStack(Material.TIPPED_ARROW);
-        PotionMeta potionMeta = (PotionMeta) item.getItemMeta();
-
-        for (PotionEffect effect : arrow.getCustomEffects()) {
-          potionMeta.addCustomEffect(effect, true);
-        }
-
-        item.setItemMeta(potionMeta);
-      } else if (projectile instanceof SpectralArrow) {
-        item = new ItemStack(Material.SPECTRAL_ARROW);
-      } else {
-        item = new ItemStack(Material.ARROW);
-      }
+    if (projectile instanceof Arrow) {
+      item = new ItemStack(Material.ARROW);
+    } else if (projectile instanceof SpectralArrow) {
+      item = new ItemStack(Material.SPECTRAL_ARROW);
     } else if (projectile instanceof ThrownPotion thrownPotion) {
       return thrownPotion.getItem();
     } else {
