ad28574c36f9ee05ba4d262b8ef4c979fe2644f0
Villagers654 <thbvill@gmail.com>
2024-02-24 18:50:58 +0900

    Prevent dummies from damaging players

--- src/main/java/club/aurorapvp/auroracombat/events/listeners/DummyCombatEventListener.java
+++ src/main/java/club/aurorapvp/auroracombat/events/listeners/DummyCombatEventListener.java
@@ -1,18 +1,23 @@
 package club.aurorapvp.auroracombat.events.listeners;
 
-import club.aurorapvp.auroracombat.events.custom.EntityDamagedByPlayerEvent;
+import club.aurorapvp.auroracombat.events.custom.EntityDamagedByEntityEvent;
 import club.aurorapvp.auroracombat.modules.PracticeDummy;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.Zombie;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 
-public class DummyDamageEventListener implements Listener {
+public class DummyCombatEventListener implements Listener {
 
   @EventHandler
-  public void onDummyDamage(EntityDamagedByPlayerEvent event) {
+  public void onDummyDamage(EntityDamagedByEntityEvent event) {
     if (!(event.getDamaged() instanceof Zombie zombie)) {
+      return;
+    }
+
+    if (!(event.getAttacker() instanceof Player)) {
       return;
     }
 
@@ -31,5 +36,18 @@ public class DummyDamageEventListener implements Listener {
 
       dummy.getZombie().getEquipment().setItemInOffHand(new ItemStack(Material.TOTEM_OF_UNDYING));
     }
+  }
+
+  @EventHandler
+  public void onDummyAttack(EntityDamagedByEntityEvent event) {
+    if (!(event.getAttacker() instanceof Zombie zombie)) {
+      return;
+    }
+
+    if (!(event.getDamaged() instanceof Player)) {
+      return;
+    }
+
+    event.setCancelled(true);
   }
 }
