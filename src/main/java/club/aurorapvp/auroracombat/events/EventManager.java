cddf827d3244f6064cac2aa18163cef60c563269
Villagers654 <thbvill@gmail.com>
2024-02-24 18:59:36 +0900

    I always forget to register listeners

--- src/main/java/club/aurorapvp/auroracombat/events/EventManager.java
+++ src/main/java/club/aurorapvp/auroracombat/events/EventManager.java
@@ -3,6 +3,7 @@ package club.aurorapvp.auroracombat.events;
 import club.aurorapvp.auroracombat.AuroraCombat;
 import club.aurorapvp.auroracombat.events.listeners.CombatEventListener;
 import club.aurorapvp.auroracombat.events.listeners.DamageEventListener;
+import club.aurorapvp.auroracombat.events.listeners.DummyCombatEventListener;
 import club.aurorapvp.auroracombat.events.listeners.PlayerEventListener;
 import club.aurorapvp.auroracombat.events.listeners.ProjectileListener;
 import org.bukkit.Bukkit;
@@ -14,5 +15,6 @@ public class EventManager {
     Bukkit.getPluginManager().registerEvents(new DamageEventListener(), AuroraCombat.getInstance());
     Bukkit.getPluginManager().registerEvents(new PlayerEventListener(), AuroraCombat.getInstance());
     Bukkit.getPluginManager().registerEvents(new ProjectileListener(), AuroraCombat.getInstance());
+    Bukkit.getPluginManager().registerEvents(new DummyCombatEventListener(), AuroraCombat.getInstance());
   }
 }
