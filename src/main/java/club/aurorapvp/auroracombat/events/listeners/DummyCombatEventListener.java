4a5293ec5d43f07261cfbe6ccc24d4c0a198296a
Villagers654 <thbvill@gmail.com>
2024-02-24 21:58:09 +0900

    Use EntityResurrectEvent instead of EntityDamagedByEntityEvent

--- src/main/java/club/aurorapvp/auroracombat/events/listeners/DummyCombatEventListener.java
+++ src/main/java/club/aurorapvp/auroracombat/events/listeners/DummyCombatEventListener.java
@@ -7,17 +7,14 @@ import org.bukkit.entity.Player;
 import org.bukkit.entity.Zombie;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityResurrectEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class DummyCombatEventListener implements Listener {
 
   @EventHandler
-  public void onDummyDamage(EntityDamagedByEntityEvent event) {
-    if (!(event.getDamaged() instanceof Zombie zombie)) {
-      return;
-    }
-
-    if (!(event.getAttacker() instanceof Player)) {
+  public void onDummyDamage(EntityResurrectEvent event) {
+    if (!(event.getEntity() instanceof Zombie zombie)) {
       return;
     }
 
@@ -32,7 +29,7 @@ public class DummyCombatEventListener implements Listener {
     }
 
     if (dummy.getInventory().contains(Material.TOTEM_OF_UNDYING)) {
-      dummy.getInventory().remove(new ItemStack(Material.TOTEM_OF_UNDYING, 1));
+      dummy.getInventory().removeItem(new ItemStack(Material.TOTEM_OF_UNDYING));
 
       dummy.getZombie().getEquipment().setItemInOffHand(new ItemStack(Material.TOTEM_OF_UNDYING));
     }
