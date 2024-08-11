89f1437902952d09b9cc0660632a79067f8e0715
Villagers654 <110007851+Villagers654@users.noreply.github.com>
2024-08-11 14:47:16 -0400

    Simplify stats command logic

--- src/main/java/club/aurorapvp/auroracombat/commands/StatsCommand.java
+++ src/main/java/club/aurorapvp/auroracombat/commands/StatsCommand.java
@@ -4,12 +4,7 @@ import club.aurorapvp.auroracombat.AuroraCombat;
 import club.aurorapvp.auroracombat.modules.KillDeathTracker;
 import club.aurorapvp.auroracombat.modules.Rating;
 import co.aikar.commands.BaseCommand;
-import co.aikar.commands.annotation.CommandAlias;
-import co.aikar.commands.annotation.CommandCompletion;
-import co.aikar.commands.annotation.CommandPermission;
-import co.aikar.commands.annotation.Default;
-import co.aikar.commands.annotation.Description;
-import co.aikar.commands.annotation.Syntax;
+import co.aikar.commands.annotation.*;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
@@ -18,44 +13,15 @@ import org.bukkit.entity.Player;
 public class StatsCommand extends BaseCommand {
 
   @Default
-  @Description("Gets your own major stats")
-  @SuppressWarnings("unused")
-  public void onGetStats(Player player) {
-    KillDeathTracker tracker = KillDeathTracker.getTracker(player);
-
-    StringBuilder ratings = new StringBuilder();
-    for (Rating rating : Rating.getRatings()) {
-      String ratingName = rating.getFriendlyName();
-      int points = rating.getScore(player).getPoints();
-      ratings
-          .append("<aqua><bold>")
-          .append(ratingName)
-          .append(": <reset><aqua>")
-          .append(points)
-          .append("<reset>\n");
-    }
-
-    player.sendMessage(
-        AuroraCombat.getInstance()
-            .getLang()
-            .formatComponent(
-                "stats-command",
-                player.getName(),
-                ratings.toString(),
-                tracker.getKills(),
-                tracker.getDeaths(),
-                tracker.getKDR()));
-  }
-
-  @Default
   @CommandCompletion("@players")
   @Syntax("<player>")
   @Description("Gets another player's major stats")
   @SuppressWarnings("unused")
-  public void onGetStats(Player sender, String playerName) {
-    Player player = Bukkit.getPlayer(playerName);
+  public void onGetStats(Player sender, @Optional String playerName) {
+    Player player = playerName == null ? sender : Bukkit.getPlayer(playerName);
 
     if (player == null) {
+      assert sender != null;
       sender.sendMessage(AuroraCombat.getInstance().getLang().getComponent("unknown-player"));
       return;
     }
